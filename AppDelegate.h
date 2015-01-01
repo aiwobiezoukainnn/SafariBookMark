c34cb6faa9fe5bb5087f54b6d9e7f1ce9a7a654e
aiwobiezoukainnn <875800307@qq.com>
2015-01-01 20:42:55 +0800

    this code ship are added to the git

--- AppDelegate.h
+++ AppDelegate.h
@@ -20,7 +20,7 @@ public:
     @return false   Initialize failed, app terminate.
     */
     virtual bool applicationDidFinishLaunching();
-
+    git rm  --cached bookmark.cpp
     /**
     @brief  The function be called when the application enter background
     @param  the pointer of the application
